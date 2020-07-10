bda8280a11b93e9a808020c8c7fd5f0beaa62c39
parsa <parsa2820@gmail.com>
2020-07-10 09:40:26 +0430

    Add GUI

--- ttf.sh
+++ ttf.sh
@@ -1,32 +1,11 @@
-: '
-██████╗  █████╗ ██████╗ ███████╗ █████╗ ██████╗  █████╗ ██████╗  ██████╗ 
-██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗╚════██╗██╔══██╗╚════██╗██╔═████╗
-██████╔╝███████║██████╔╝███████╗███████║ █████╔╝╚█████╔╝ █████╔╝██║██╔██║
-██╔═══╝ ██╔══██║██╔══██╗╚════██║██╔══██║██╔═══╝ ██╔══██╗██╔═══╝ ████╔╝██║
-██║     ██║  ██║██║  ██║███████║██║  ██║███████╗╚█████╔╝███████╗╚██████╔╝
-╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝ ╚════╝ ╚══════╝ ╚═════╝ 
-'
 IFS=$'\n'
-read -p "Select program mode :
-1 - Find time of the entered folder
-2 - Find time of the entered folder and all subfolders
-" mode
-read -p "Enter directory:" dir
-if [ $mode == "1" ];then
-	dirlist=$dir
-fi
-if [ $mode == "2" ];then
-	dirlist=`ls -1 -d $dir/*`
-fi
-realtotaldur=0
-for folder in $dirlist;do
-	cd $folder
-	list=`ls -1`
-	totaldur=0
-	for file in $list;do
-		filedur=`ffprobe -v quiet -show_entries format=duration -of csv="p=0" "$file" | sed 's/\..*//g'`
-		totaldur=$((totaldur+filedur))
+dir=`zenity --file-selection --title "TTF - Choose a directory" --window-icon=icon.png --directory`
+cd $dir
+list=`ls -1 *.{webm,mkv,vob,mov,avi,mp4,m4v,m4p,3gp,flv,mp3,flac,m4a} 2> /dev/null`
+totaldur=0
+for file in $list;do
+	filedur=`ffprobe -v quiet -show_entries format=duration -of csv="p=0" "$file" | sed 's/\..*//g'`
+	totaldur=$((totaldur+filedur))
 done
-realtotaldur=$((realtotaldur+totaldur))
-done
-echo "$((realtotaldur/60)) Minutes"
+result="$((totaldur/60)) Minutes"
+zenity --info --title "TTF - Result" --window-icon=icon.png --text "$result Minutes"
